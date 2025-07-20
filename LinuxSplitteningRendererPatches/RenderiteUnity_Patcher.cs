b3f7af7d9b8dfc6653921162e5a33af7c2380ed1
Baplar <baplar@gmail.com>
2025-07-20 22:28:36 +0200

    Fixup shutdown and update interprocess submodule

--- LinuxSplitteningRendererPatches/RenderiteUnity_Patcher.cs
+++ LinuxSplitteningRendererPatches/RenderiteUnity_Patcher.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-using System.IO;
 using HarmonyLib;
 using Renderite.Unity;
 
@@ -8,11 +6,9 @@ namespace LinuxSplitteningRendererPatches;
 [HarmonyPatch(typeof(RenderingManager), "HasMainProcessExited", MethodType.Getter)]
 public static class HasMainProcessExited_Patch {
 
-    public static void Postfix(ref RenderingManager __instance, ref bool __result) {
-        // If main process is actually managed, use it anyway
-        if (Renderite.Shared.Helper.IsWine && __instance.MainProcess != null)
-        {
-            __result = __instance.MainProcess.HasExited;
-        }
+    public static bool Prefix(ref RenderingManager __instance, ref bool __result)
+    {
+        __result = __instance.MainProcess.HasExited;
+        return false;
     }
 }
