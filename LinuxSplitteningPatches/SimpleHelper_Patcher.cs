a7e82304fa907ad791e9c451bb9a960ed56a6d5b
Baplar <baplar@gmail.com>
2025-07-24 06:07:58 +0200

    Add changes from Interprocess and minor changes

--- LinuxSplitteningPatches/SimpleHelper_Patcher.cs
+++ LinuxSplitteningPatches/SimpleHelper_Patcher.cs
@@ -2,8 +2,6 @@ using System;
 using System.Text;
 using HarmonyLib;
 using Elements.Core;
-using ResoniteModLoader;
-using Org.BouncyCastle.Utilities.Encoders;
 
 namespace LinuxSplitteningPatches;
 
@@ -12,11 +10,6 @@ namespace LinuxSplitteningPatches;
 public static class EncryptPatch {
     public static bool Prefix(this string text, byte[] key, byte[] iv, ref string __result) {
         __result = Convert.ToBase64String(DESCrypto.Encrypt(text, key, iv));
-
-        var keyStr = Encoding.UTF8.GetString(Hex.Encode(key));
-        var ivStr = Encoding.UTF8.GetString(Hex.Encode(iv));
-
-        ResoniteMod.Debug("SimpleEncrypt", text, keyStr, ivStr, __result);
         return false;
     }
 }
@@ -25,13 +18,7 @@ public static class EncryptPatch {
 [HarmonyPatch(nameof(StringHelper.SimpleDecrypt))]
 public static class DecryptPatch {
     public static bool Prefix(this string text, byte[] key, byte[] iv, ref string __result) {
-
         __result = Encoding.Unicode.GetString(DESCrypto.Decrypt(text, key, iv));
-
-        var keyStr = Encoding.UTF8.GetString(Hex.Encode(key));
-        var ivStr = Encoding.UTF8.GetString(Hex.Encode(iv));
-
-        ResoniteMod.Debug("SimpleDecrypt", text, keyStr, ivStr, __result, "");
         return false;
     }
 }
--- LinuxSplitteningRendererPatches/RenderiteUnity_Patcher.cs
+++ LinuxSplitteningRendererPatches/RenderiteUnity_Patcher.cs
@@ -1,14 +1,36 @@
 using HarmonyLib;
 using Renderite.Unity;
+using UnityEngine;
 
 namespace LinuxSplitteningRendererPatches;
 
 [HarmonyPatch(typeof(RenderingManager), "HasMainProcessExited", MethodType.Getter)]
-public static class HasMainProcessExited_Patch {
+public static class HasMainProcessExited_Patch
+{
+    private static bool hasSearchedWineProcess = false;
 
-    public static bool Prefix(ref RenderingManager __instance, ref bool __result)
+    public static void Postfix(ref RenderingManager __instance, ref bool __result, int ____mainProcessId)
     {
-        __result = __instance.MainProcess.HasExited;
-        return false;
+        if (!hasSearchedWineProcess)
+        {
+            if (__instance.MainProcess == null)
+            {
+                Debug.LogWarning($"MainProcess was null, searching id {____mainProcessId} in case main process is on Wine too");
+                try
+                {
+                    AccessTools.PropertySetter(typeof(RenderingManager), "MainProcess").Invoke(__instance, [System.Diagnostics.Process.GetProcessById(____mainProcessId)]);
+                    Debug.Log("MainProcess was found, we are probably on a full Wine setup");
+                }
+                catch
+                {
+                    Debug.Log("Could not find process by ID, we are on a hybrid Native/Wine setup");
+                }
+            }
+            hasSearchedWineProcess = true;
+        }
+        if (Renderite.Shared.Helper.IsWine && __instance.MainProcess != null)
+        {
+            __result &= __instance.MainProcess.HasExited;
+        }
     }
 }
